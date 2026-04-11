1a5de56b809abe13510cde0a4e8fcd4c583d6708
yenifervalencia <yenifer.v.e2909@gmail.com>
2026-04-10 19:36:13 -0500

    create component and define routes

---
+++ src/app/modules/main/events/detail/detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail',
+  imports: [],
+  templateUrl: './detail.component.html',
+  styleUrl: './detail.component.css',
+  standalone: true,
+})
+export class DetailComponent {
+
+}
